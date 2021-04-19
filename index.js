8d828b7cd25ae7738b5316a654cb740baf860238
unknown <pushpitarathore2106@gmail.com>
2021-04-19 16:56:28 +0530

    javascript callback function

--- index.js
+++ index.js
@@ -4,18 +4,18 @@ function sayName(name,callback){
     console.log("hello " + name);
     callback("pushpita");
 }
-function sayhow(name){
+function sayhow(name){  //formal argument
     console.log("how are you " + name);
 }
 //call the function
-sayName("pushpita",sayhow);
+sayName("pushpita",sayhow);  //actual argument
 
 function hello(){
     console.log("hello wold");
 }
-function sayHii(n){
+function sayHii(n){ //formal argument
     console.log("hii "+ n);
 }
 //call the function
 setTimeout(hello,2000); //2000 = 2sec
-sayHii("pushpita");
+sayHii("pushpita"); //actual argument
